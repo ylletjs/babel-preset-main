67093927adca691fa7021843eba9019342b65905
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-12-06 21:41:31 +0100

    Bump version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Modeled on create-react-app preset
 // https://github.com/facebook/create-react-app/tree/master/packages/babel-preset-react-app
 
-module.exports = function() {
+module.exports = function () {
   return {
     presets: [
       [
